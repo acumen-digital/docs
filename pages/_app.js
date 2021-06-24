73e55325101400824c666a678a98c8e7597922d0
Michael Okoh <ordrizzy@gmail.com>
2021-06-24 23:33:03 +0100

    house keeping

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import 'nextra-theme-docs/style.css'
+import Prism from 'prism-react-renderer/prism'
+
+(typeof global !== "undefined" ? global : window).Prism = Prism
+require("prismjs/components/prism-php")
+require("prismjs/components/prism-javascript")
+require("prismjs/components/prism-json")
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />
